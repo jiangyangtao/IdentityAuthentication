cea472f3a83b2cffc9a5607a09f992bec4e8cf1e
Yangtao Jiang <jiangyangtao1@qq.com>
2022-11-17 23:00:22 +0800

    Add http request the extensions

---
+++ src/IdentityAuthentication.Model/Extensions/RequestExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http.Headers;
+
+namespace IdentityAuthentication.Model.Extensions
+{
+    public static class RequestExtensions
+    {
+        public static void SetRefreshToken(this HttpRequestHeaders headers, string refreshToken) => headers.SetValue(HttpHeaderKeyDefaults.RefreToken, refreshToken);
+
+        public static void SetAccessToken(this HttpRequestHeaders headers, string accessToken) => headers.SetValue(HttpHeaderKeyDefaults.AccessToken, accessToken);
+
+        public static void SetAuthorization(this HttpRequestHeaders headers, string authorization) => headers.SetValue(HttpHeaderKeyDefaults.Authorization, authorization);
+
+        public static void SetValue(this HttpRequestHeaders headers, string key, string value)
+        {
+            if (headers == null) return;
+            if (key.IsNullOrEmpty()) return;
+            if (value.IsNullOrEmpty()) return;
+
+            if (headers.Contains(key))
+            {
+                headers.Authorization = new AuthenticationHeaderValue(value);
+                return;
+            }
+
+            headers.Add(key, value);
+        }
+    }
+}
