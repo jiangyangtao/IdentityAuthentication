f6edb63ee00e76dd69317144c1bcad76bf0a36d7
Yangtao Jiang <jiangyangtao1@qq.com>
2022-12-25 01:42:15 +0800

    Add employee the project

---
+++ src/Authentications/Authentication.Employee/EmployeeAuthenticationInitialization.cs
@@ -0,0 +1,19 @@
+﻿using Authentication.Abstractions.Credentials;
+using Authentication.Abstractions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Authentication.Employee
+{
+    public class EmployeeAuthenticationInitialization : IAuthenticationInitialization
+    {
+        public IApplicationBuilder InitializationApplication(IApplicationBuilder builder) => builder;
+
+        public IServiceCollection InitializationService(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<IAuthenticationService<PasswordCredential>, EmployeeAuthenticationService>();
+            return services;
+        }
+    }
+}
---
+++ src/Authentications/Authentication.Employee/EmployeeAuthenticationService.cs
@@ -0,0 +1,20 @@
+﻿using Authentication.Abstractions;
+using Authentication.Abstractions.Credentials;
+
+namespace Authentication.Employee
+{
+    internal class EmployeeAuthenticationService : IAuthenticationService<PasswordCredential>
+    {
+        public string GrantSource => "Employee";
+
+        public Task<AuthenticationResult> AuthenticateAsync(PasswordCredential credential)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IdentityCheckAsync(string id, string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
