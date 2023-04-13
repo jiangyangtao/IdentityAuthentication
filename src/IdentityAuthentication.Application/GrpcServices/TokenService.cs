5173ca2ad58a5d64f74f8443b13f4d9d899ca054
Yangtao Jiang <jiangyangtao1@qq.com>
2023-04-13 19:23:03 +0800

    Remove Generate

--- src/IdentityAuthentication.Application/GrpcServices/TokenService.cs
+++ src/IdentityAuthentication.Application/GrpcServices/TokenService.cs
@@ -39,10 +39,5 @@ namespace IdentityAuthentication.Application.GrpcServices
             var token = await _authenticaionProvider.RefreshTokenAsync();
             return new RefreshTokenResponse { AccessToken = token, Result = token.NotNullAndEmpty() };
         }
-
-        public override Task<GenerateTokenResponse> Generate(GenerateTokenRequest request, ServerCallContext context)
-        {
-            return base.Generate(request, context);
-        }
     }
 }
