c27835904f6c7a29e2fa3cee4b57da073882747d
Yangtao Jiang <jiangyangtao1@qq.com>
2023-09-17 05:03:08 +0800

    Add cahe to DI support

---
+++ src/IdentityAuthentication.Cache/CacheProviderFactory.cs
@@ -0,0 +1,23 @@
+﻿using IdentityAuthentication.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace IdentityAuthentication.Cache
+{
+    internal class CacheProviderFactory : ICacheProviderFactory
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly CacheStorageConfiguration _cacheStorageConfiguration;
+
+        public CacheProviderFactory(IServiceProvider serviceProvider, IOptions<CacheStorageConfiguration> options)
+        {
+            _serviceProvider = serviceProvider;
+            _cacheStorageConfiguration = options.Value;
+        }
+
+        public ICacheProvider CreateCacheProvider()
+        {
+            return _serviceProvider.GetServices<ICacheProvider>().FirstOrDefault(a => a.StorageType == _cacheStorageConfiguration.StorageType);
+        }
+    }
+}
---
+++ src/IdentityAuthentication.Cache/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using IdentityAuthentication.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityAuthentication.Cache
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCacheStorage(this IServiceCollection services)
+        {
+            var serviceProvider = services.BuildServiceProvider();
+            var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+
+            services.Configure<CacheStorageConfiguration>(configuration.GetSection("CacheStorage"));
+
+            services.AddSingleton<ICacheProvider, MemoryCacheProvider>();
+            services.AddSingleton<ICacheProvider, RedisCacheProvider>();
+            services.AddSingleton<ICacheProviderFactory, CacheProviderFactory>();
+
+            return services;
+        }
+    }
+}
