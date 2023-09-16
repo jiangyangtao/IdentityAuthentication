15f330d7c5457c2b7699b04f4cbd2963f001c685
Yangtao Jiang <jiangyangtao1@qq.com>
2023-09-17 04:33:37 +0800

    Add SymmetricSignatureConfiguration

---
+++ src/IdentityAuthentication.Configuration/SymmetricSignatureConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using IdentityAuthentication.Configuration.Enums;
+using IdentityAuthentication.Model.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace IdentityAuthentication.Configuration
+{
+    public class SymmetricSignatureConfiguration : SymmetricSignature
+    {
+        public JwtSignatureAlgorithm SignatureAlgorithm { set; get; }
+
+        public override string Algorithm
+        {
+            get
+            {
+                if (SignatureAlgorithm == JwtSignatureAlgorithm.HmacSha256) return SecurityAlgorithms.HmacSha256;
+                if (SignatureAlgorithm == JwtSignatureAlgorithm.HmacSha384) return SecurityAlgorithms.HmacSha384;
+
+                return SecurityAlgorithms.HmacSha512;
+            }
+        }
+    }
+}
--- src/IdentityAuthentication.Model/Models/SymmetricSignature.cs
+++ src/IdentityAuthentication.Model/Models/SymmetricSignature.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace IdentityAuthentication.Model.Models
 {
     public abstract class SymmetricSignature
     {
         public string SignatureKey { set; get; }
 
-        public string Algorithm { set; get; }
+        public abstract string Algorithm { get; }
     }
 }
