2edcb563be12ae79c2e022434ea3a54ebe47c192
Yangtao Jiang <jiangyangtao1@qq.com>
2023-09-16 19:51:26 +0800

    Fixed Token the bugs

--- src/IdentityAuthentication.Token/EncryptionTokenProvider.cs
+++ src/IdentityAuthentication.Token/EncryptionTokenProvider.cs
@@ -1,4 +1,7 @@
-﻿using IdentityAuthentication.Token.Abstractions;
+﻿using IdentityAuthentication.Configuration.Model;
+using IdentityAuthentication.Model.Enums;
+using IdentityAuthentication.Token.Abstractions;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +10,42 @@ using System.Threading.Tasks;
 
 namespace IdentityAuthentication.Token
 {
-    internal class EncryptionTokenProvider: ITokenProvider
+    internal class EncryptionTokenProvider : ITokenProvider
     {
+        public EncryptionTokenProvider()
+        {
+        }
+
+        public TokenType TokenType => throw new NotImplementedException();
+
+        public Task<TokenValidationResult> AuthorizeAsync(string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> DestroyAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ITokenResult> GenerateAsync(AuthenticationResult authenticationResult)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AuthenticationResult> GetAuthenticationResultAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyDictionary<string, string>?> GetTokenInfoAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> RefreshAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- src/IdentityAuthentication.Token/JwtTokenProvider.cs
+++ src/IdentityAuthentication.Token/JwtTokenProvider.cs
@@ -1,9 +1,46 @@
-﻿using IdentityAuthentication.Token.Abstractions;
+﻿using IdentityAuthentication.Configuration.Model;
+using IdentityAuthentication.Model.Enums;
+using IdentityAuthentication.Token.Abstractions;
+using Microsoft.IdentityModel.Tokens;
 
 namespace IdentityAuthentication.Token
 {
-    public class JwtTokenProvider: ITokenProvider
+    public class JwtTokenProvider : ITokenProvider
     {
+        public JwtTokenProvider()
+        {
+        }
 
+        public TokenType TokenType => throw new NotImplementedException();
+
+        public Task<TokenValidationResult> AuthorizeAsync(string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> DestroyAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ITokenResult> GenerateAsync(AuthenticationResult authenticationResult)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AuthenticationResult> GetAuthenticationResultAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyDictionary<string, string>?> GetTokenInfoAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> RefreshAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
