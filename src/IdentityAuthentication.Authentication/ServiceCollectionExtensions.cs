2794487b7d9fdf25e45e02acd1c889f9980c8e5c
Yangtao Jiang <jiangyangtao1@qq.com>
2023-09-16 20:35:33 +0800

    Add DI support

--- src/IdentityAuthentication.Authentication/ServiceCollectionExtensions.cs
+++ src/IdentityAuthentication.Authentication/ServiceCollectionExtensions.cs
@@ -1,8 +1,51 @@
 ﻿
+using IdentityAuthentication.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace IdentityAuthentication.Authentication
 {
     public static class ServiceCollectionExtensions
     {
+        public static IServiceCollection AddAuthentication(this IServiceCollection services)
+        {
+            services.AddSingleton<AuthenticationHandle>();
+            services.AddSingleton<IAuthenticationProvider, AuthenticationProvider>();
 
+            var serviceProvider = services.BuildServiceProvider();
+            var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+            services.AddGrantSources(configuration);
+
+            return services;
+        }
+
+        private static IServiceCollection AddGrantSources(this IServiceCollection services, IConfiguration configuration)
+        {
+            var authenticationInitializations = AssemblyProvider.AuthenticationInitializations;
+            if (authenticationInitializations.NotNullAndEmpty())
+            {
+                foreach (var authenticationInitialization in authenticationInitializations)
+                {
+                    authenticationInitialization?.InitializationService(services, configuration);
+                }
+            }
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseIdentityAuthentication(this IApplicationBuilder builder)
+        {
+            var authenticationInitializations = AssemblyProvider.AuthenticationInitializations;
+            if (authenticationInitializations.NotNullAndEmpty())
+            {
+                foreach (var authenticationInitialization in authenticationInitializations)
+                {
+                    authenticationInitialization?.InitializationApplication(builder);
+                }
+            }
+
+            return builder;
+        }
     }
 }
--- src/IdentityAuthentication.Core/ServiceCollectionExtensions.cs
+++ src/IdentityAuthentication.Core/ServiceCollectionExtensions.cs
@@ -17,46 +17,13 @@ namespace IdentityAuthentication.Core
             var serviceProvider = services.BuildServiceProvider();
             var configuration = serviceProvider.GetRequiredService<IConfiguration>();
 
-            services.AddGrantSources(configuration);
             services.Configure<GrantDefaultConfiguration>(configuration.GetSection("GrantDefaults"));
             services.Configure<AccessTokenConfiguration>(configuration.GetSection("AccessToken"));
             services.Configure<RefreshTokenConfiguration>(configuration.GetSection("RefreshToken"));
             services.Configure<AuthenticationConfiguration>(configuration.GetSection("Authentication"));
 
-            services.AddSingleton<AuthenticationHandle>();
             services.AddSingleton<IAuthenticationHandler, AuthenticationHandldr>();
-
-            services.AddTokenSerivces(configuration);
-
             return services;
-        }
-
-        private static IServiceCollection AddGrantSources(this IServiceCollection services, IConfiguration configuration)
-        {
-            var authenticationInitializations = AssemblyProvider.AuthenticationInitializations;
-            if (authenticationInitializations.NotNullAndEmpty())
-            {
-                foreach (var authenticationInitialization in authenticationInitializations)
-                {
-                    authenticationInitialization?.InitializationService(services, configuration);
-                }
-            }
-
-            return services;
-        }
-
-        public static IApplicationBuilder UseIdentityAuthentication(this IApplicationBuilder builder)
-        {
-            var authenticationInitializations = AssemblyProvider.AuthenticationInitializations;
-            if (authenticationInitializations.NotNullAndEmpty())
-            {
-                foreach (var authenticationInitialization in authenticationInitializations)
-                {
-                    authenticationInitialization?.InitializationApplication(builder);
-                }
-            }
-
-            return builder;
         }
     }
 }
