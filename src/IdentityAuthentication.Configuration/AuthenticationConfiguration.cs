ad3203bb9f4c794cbcc972b72817a0dffc9f7eb1
Yangtao Jiang <jiangyangtao1@qq.com>
2023-09-18 01:23:51 +0800

    Update config

--- src/IdentityAuthentication.Configuration/AuthenticationConfiguration.cs
+++ src/IdentityAuthentication.Configuration/AuthenticationConfiguration.cs
@@ -4,12 +4,7 @@ using IdentityAuthentication.Model.Configurations;
 namespace IdentityAuthentication.Configuration
 {
     public class AuthenticationConfiguration : AuthenticationConfigurationBase
-    {
-        /// <summary>
-        /// 启用 token 刷新
-        /// </summary>
-        public bool EnableTokenRefresh { set; get; }
-
+    {    
         public TokenSignatureType TokenSignatureType { set; get; }
 
         public TokenEncryptionType TokenEncryptionType { set; get; }
--- src/IdentityAuthentication.Model/Configurations/AuthenticationConfigurationBase.cs
+++ src/IdentityAuthentication.Model/Configurations/AuthenticationConfigurationBase.cs
@@ -6,6 +6,14 @@ namespace IdentityAuthentication.Model.Configurations
     {
         public TokenType TokenType { set; get; } = TokenType.JWT;
 
+        /// <summary>
+        /// 启用 token 刷新
+        /// </summary>
+        public bool EnableTokenRefresh { set; get; }
+
+        /// <summary>
+        /// 启用 Grpc 连接
+        /// </summary>
         public bool EnableGrpcConnection { get; set; }
     }
 }
