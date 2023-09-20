707b73990032ad8a95586276a4034e4a5d0bb888
Yangtao Jiang <jiangyangtao1@qq.com>
2023-09-21 01:02:01 +0800

    Add IAuthenticationResult

---
+++ src/IdentityAuthentication.Configuration/Abstractions/IAuthenticationResult.cs
@@ -0,0 +1,28 @@
+﻿namespace IdentityAuthentication.Configuration.Abstractions
+{
+    public interface IAuthenticationResult
+    {
+        static string UserIdPropertyName => nameof(UserId);
+
+        static string UsernamePropertyName => nameof(Username);
+
+        static string GrantSourcePropertyName => nameof(GrantSource);
+
+        static string GrantTypePropertyName => nameof(GrantType);
+
+        static string ClientPropertyName => nameof(Client);
+
+
+        string UserId { get; }
+
+        string Username { get; }
+
+        string GrantSource { get; }
+
+        string GrantType { get; }
+
+        string Client { get; }
+
+        IReadOnlyDictionary<string, string> Metadata { get; }
+    }
+}
